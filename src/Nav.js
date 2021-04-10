5618bd8ace807c31f2546a322ad837db087d1e08
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2021-04-10 10:59:22 +0900

    新規ファイルの追加

---
+++ src/Nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+
+function Nav() {
+return (
+  <nav>
+    <h3>Logo</h3>
+    <ul className="nav-links">
+      <li>About</li>
+      <li>Shop</li>
+    </ul>
+    </nav>
+  );
+}
+
+export default Nav;
---
+++ src/Shop.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+
+function Shop() {
+  return (
+    <div>
+      <h1>Shop Page</h1>
+    </div>
+  );
+}
+
+export default Shop;
